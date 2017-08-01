abbf405901d9a6c1e8bd034956e44cc2bb9bcedd
abc02 <abcqq02@qq.com>
2017-08-01 23:36:50 +0800

    预览排版

--- src/components/Preview.vue
+++ src/components/Preview.vue
@@ -1,43 +1,56 @@
 <template>
     <div id="preview">
-        <h1>{{ resume.profile.name || '请填写姓名'}}</h1>
-        <p>{{ resume.profile.citry || '请填写城市'}} | {{ resume.profile.birth || '请填写出生年月'}}</p>
+        <section class="profile">
+            <h1>{{ resume.profile.name || '请填写姓名'}}</h1>
+            <p>
+                <strong>{{ resume.profile.citry || '请填写城市'}} </strong>|
+                <small>{{ resume.profile.birth || '请填写出生年月'}}</small>
+            </p>
+        </section>
+        <div class="container">
     
-        <section v-if="filter(resume.workHistory).length > 0">
-            <h2>工作经历</h2>
-            <ul>
-                <li v-for="work in resume.workHistory">
-                    {{ work.company}} {{work.content}}
-                </li>
-            </ul>
-        </section>
-        <section v-if="filter(resume.projects).length > 0">
-            <h2>前端项目</h2>
-            <ul>
-                <li v-for="item in filter(resume.projects)">
-                    {{item.name}} {{item.content}}
-                </li>
-            </ul>
-        </section>
-        <section v-if="filter(resume.school).length > 0">
-            <h2>学习经历</h2>
-            <ul>
-                <li v-for="item in filter(resume.school)">
-                    {{item.name}} {{item.major}}
-                </li>
-            </ul>
-        </section>
-        <section>
-            <h2>联系方式</h2>
-            <p>手机:
-                <span>{{resume.contact.phone || '请填写手机号码'}}</span>
-            </p>微信:
-            <span>{{resume.contact.wechat || '请填写微信号码'}}</span>
-            </p>
-            <p>邮箱:
-                <span>{{resume.contact.email || '请填写电子邮箱'}}</span>
-            </p>
-        </section>
+            <section v-if="filter(resume.workHistory).length > 0" class="workHistory">
+                <h2>工作经历</h2>
+                <ul>
+                    <li v-for="work in resume.workHistory">
+                        <h3>{{ work.company}} </h3>
+                        <span>{{work.content}}</span>
+                    </li>
+                </ul>
+            </section>
+            <section v-if="filter(resume.projects).length > 0" class="projects">
+                <h2>前端项目</h2>
+                <ul>
+                    <li v-for="item in filter(resume.projects)">
+                        <h3>{{item.name}} </h3>
+                        <span>{{item.content}}</span>
+                    </li>
+                </ul>
+            </section>
+            <div class="split"></div>
+            <section v-if="filter(resume.school).length > 0" class="school">
+                <h2>学习经历</h2>
+                <ul>
+                    <li v-for="item in filter(resume.school)">
+                        <h3>{{item.name}}</h3>
+                        <span> {{item.major}}</span>
+                    </li>
+                </ul>
+            </section>
+    
+            <section class="contact">
+                <h2>联系方式</h2>
+                <p>手机:
+                    <span>{{resume.contact.phone || '请填写手机号码'}}</span>
+                </p>
+                <p>微信:
+                    <span>{{resume.contact.wechat || '请填写微信号码'}}</span>
+                </p>
+                <p>邮箱:
+                    <span>{{resume.contact.email || '请填写电子邮箱'}}</span>
+                </p>
+            </section>
+        </div>
     </div>
 </template>
 
@@ -63,7 +76,33 @@ export default {
 </script>
 <style lang="scss">
 #preview {
+    h1,
+    h2 {
+        padding: 0;
+        margin: 0;
+    }
     padding: 32px;
-    h1 {}
+    section {
+        margin: 0 16px 32px 0;
+        padding-bottom: 8px;
+        border-bottom: 1px solid #ddd;
+        li,
+        p {
+            padding: 8px;
+            h2,
+            h3 {
+                padding-bottom: 8px;
+            }
+        }
+    }
+    .profile {
+        h1 {
+            margin-bottom: 8px;
+        }
+    }
+    .container {
+        display: flex;
+        flex-direction: column;
+    }
 }
 </style>
